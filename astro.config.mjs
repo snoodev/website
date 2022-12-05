10763ab867d494f1c5c4b1c63d614277c319619a
stefan-yas <stefan.sy.yas@gmail.com>
2022-12-05 13:37:38 +0100

    feat: mobile styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://snoodev.com'
-  })
-  
+  site: "https://snoodev.com",
+});
