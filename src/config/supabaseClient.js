7ef76fadc6050df6e35d92e0043246204a14f648
stefan-yas <stefan.sy.yas@gmail.com>
2022-12-06 07:07:01 +0100

    fix: form is now functional

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log(process.env.SUPABASE_URL);
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = "https://agsobcagbqbsbdbrvggl.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc29iY2FnYnFic2JkYnJ2Z2dsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njk0Nzk3MzUsImV4cCI6MTk4NTA1NTczNX0.xGmugCms2lRbf_1hA8JyvUN-wnFcGhGVzH6pKAsBlAo";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
